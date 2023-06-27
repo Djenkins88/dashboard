f7ab875e7bdf834d355bef9ff0706054381e1851
Dominque Jenkins <dominquejenkins14@gmail.com>
2023-06-26 19:13:38 -0500

    created and styled sidebar

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import  { BrowserRouter, Routes, Route } from 'react-router-dom';
 import  { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { useStateContext } from './contexts/ContextProvider';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components/Index'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages/Index'
 import './App.css';
 
 function App() {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+
 
 
   return (
